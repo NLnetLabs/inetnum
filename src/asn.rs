c5b8a9c217a181ac0ad307d96d021315af392937
Terts Diepraam <terts.diepraam@gmail.com>
2024-11-19 16:32:06 +0100

    make Asn repr(transparent)

--- src/asn.rs
+++ src/asn.rs
@@ -15,6 +15,7 @@ use octseq::builder::OctetsBuilder;
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[repr(transparent)]
 pub struct Asn(u32);
 
 impl Asn {
